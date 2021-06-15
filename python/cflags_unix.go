d97c9b506543bcad7d0d6502428a0db39fca9216
Leyuan Pan <leyuanpan@hotmail.com>
2021-06-14 22:52:11 -0700

    Update cgo flags for general support

--- python/cflags_unix.go
+++ python/cflags_unix.go
@@ -3,10 +3,7 @@
 
 package python
 
-// #cgo pkg-config: python-3.8
-// #cgo CFLAGS: -g -O3 -fPIC -ffast-math
-// #cgo CFLAGS: -I${SRCDIR}/3rd/include/python3.8
-// #cgo LDFLAGS: -L${SRCDIR}/3rd/lib -Wl,-rpath,${SRCDIR}/3rd/lib -lpython3.8
+// #cgo pkg-config: python3-embed
 // #include "py.h"
 import "C"
 
